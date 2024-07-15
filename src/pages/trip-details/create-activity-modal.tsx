710c7e0ffa01c54d8645b20244173472c8347d79
fernandoberbel <fberbel1000@gmail.com>
2024-07-14 23:56:18 -0300

    feat: ensure the creation of activities and invitation of guests on trip details page

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,11 +1,34 @@
 import { X, Tag, Calendar } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void;
 }
 
-export function CreateActivityModal({closeCreateActivityModal}: CreateActivityModalProps) {
+export function CreateActivityModal({
+  closeCreateActivityModal,
+}: CreateActivityModalProps) {
+  const { tripId } = useParams();
+
+  async function createActivity(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title")?.toString();
+    const occurs_at = data.get("occurs_at")?.toString();
+
+    await api.post(`/trips/${tripId}/activities`, {
+      title,
+      occurs_at,
+    });
+
+    window.document.location.reload() // fica como tarefa descobrir a melhor forma de fazer o reload da página
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -21,11 +44,12 @@ export function CreateActivityModal({closeCreateActivityModal}: CreateActivityMo
           </p>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={createActivity} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
               name="title"
+              type="title"
               placeholder="Qual a atividade?"
               className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
             />
